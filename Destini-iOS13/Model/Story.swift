be2302def693d1b1d3e5e9855149854283815ed5
njdeane <njdeane7@gmail.com>
2020-12-09 18:05:18 +1000

    Deleted code to implement MVC design from scratch

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,9 +9,11 @@
 import Foundation
 
 struct Story {
-    var title: String
-    var choice1: String
-    var choice2: String
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
 }
 
-let story = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
+
