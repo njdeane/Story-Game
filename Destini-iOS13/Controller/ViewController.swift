6bc53cd5dd985616ea37e6fff9bb1c94496e9f99
njdeane <njdeane7@gmail.com>
2020-12-09 18:59:19 +1000

    Control flow of story choice buttons with if else statement, will add variable INT to keep DRY

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."), // this needs to be the load default
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let stories = [
-            Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
-            Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-            Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-        ]
         
         storyLabel.text = stories[0].title
         choice1Button.setTitle(stories[0].choice1, for: .normal)
@@ -29,7 +29,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        
+        print(sender.currentTitle)
+        if sender.currentTitle == "Take a left." {
+            storyLabel.text = stories[1].title
+            choice1Button.setTitle(stories[1].choice1, for: .normal)
+            choice2Button.setTitle(stories[1].choice2, for: .normal)
+        } else {
+            storyLabel.text = stories[2].title
+            choice1Button.setTitle(stories[2].choice1, for: .normal)
+            choice2Button.setTitle(stories[2].choice2, for: .normal)
+        }
         
     }
 
